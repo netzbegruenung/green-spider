ea05bf5192916d9fa10b22d83672fac68e1b244b
Marian Steinbach <marian@giantswarm.io>
2018-04-19 00:51:59 +0200

    Move test criteria logic into spider, add scoring

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -18,54 +18,65 @@ $(function(){
 
         var row = $(document.createElement('tr'));
 
+        // typ
+        var level = null;
+        if (item.meta.level === 'DE:ORTSVERBAND') {
+          level = 'OV';
+        } else if (item.meta.level === 'DE:KREISVERBAND') {
+          level = 'KV';
+        } else if (item.meta.level === 'DE:LANDESVERBAND') {
+          level = 'LV';
+        }
+        row.append('<td>' + (level === null ? '' : level) + '</td>');
+
+        // land
+        row.append('<td>' + (item.meta.state === null ? '' : item.meta.state) + '</td>');
+
+        // kreis
+        row.append('<td>' + (item.meta.district === null ? '' : item.meta.district) + '</td>');
+
+        // stadt
+        row.append('<td>' + (item.meta.city === null ? '' : item.meta.city) + '</td>');
+
         // input URL
         row.append('<td><a href="' + item.input_url + '">' + punycode.toUnicode(item.input_url) + '</a></td>');
 
+        // score
+        row.append('<td>' + item.score.toFixed(1) + '</td>');
+
         // IPs
-        var ips = _.join(_.uniq(_.flatten(_.map(item.hostnames, 'ip_addresses'))), ', ');
-        row.append('<td class="'+ (ips === '' ? 'bad' : 'good') +' text-center">' + (ips === '' ? '❌  Keine' : ips) + '</td>');
+        var ips = _.join(item.details.ipv4_addresses, ', ');
+        row.append('<td class="'+ (ips === '' ? 'bad' : 'good') +' text-center">' + (ips === '' ? '❌' : ips) + '</td>');
 
-        // icon
-        var icons = [];
-        var icon = false;
-        icons = _.uniq(_.map(item.urlchecks, 'content.icon'));
-        if (icons.length > 0 && icons[0]) {
-          icon = icons[0];
-        }
-        row.append('<td class="' + (icon ? 'good' : 'bad') + ' text-center">' + (icon ? ('<img src="' + icon + '" class="icon"/>') : '❌') + '</td>');
+        // SITE_REACHABLE
+        row.append('<td class="'+ (item.result.SITE_REACHABLE.value ? 'good' : 'bad') +' text-center">' + (item.result.SITE_REACHABLE.value ? '✅' : '❌') + '</td>');
 
-        // hostnames
-        var twoHostnames = false;
-        if (_.filter(item.hostnames, {'resolvable': true}).length === 2) {
-          twoHostnames = true;
-        };
-        row.append('<td class="'+ (twoHostnames ? 'good' : 'bad') +' text-center">' + (twoHostnames ? '✅' : '❌') + '</td>');
+        // FAVICON
+        var icon = item.result.FAVICON.value;
+        row.append('<td class="' + (icon ? 'good' : 'bad') + ' text-center">' + (icon ? ('<img src="' + item.details.icons[0] + '" class="icon">') : '❌') + '</td>');
+
+        // WWW_OPTIONAL
+        var wwwOptional = item.result.WWW_OPTIONAL.value;
+        row.append('<td class="'+ (wwwOptional ? 'good' : 'bad') +' text-center">' + (wwwOptional ? '✅' : '❌') + '</td>');
 
         // one canonical URL
-        var canonical = false;
-        if (item.canonical_urls.length === 1) canonical = true;
-        var canonical_links = _.uniq(_.map(item.urlchecks, 'content.canonical_link'));
-        if (canonical_links.length === 1) canonical = true;
+        var canonical = item.result.CANONICAL_URL.value;
         row.append('<td class="'+ (canonical ? 'good' : 'bad') +' text-center">' + (canonical ? '✅' : '❌') + '</td>');
 
         // https
-        var hasHTTPS = false;
-        hasHTTPS = _.find(item.canonical_urls, function(o){
-          return o.indexOf('https://') !== -1;
-        });
+        var hasHTTPS = item.result.HTTPS.value;
         row.append('<td class="'+ (hasHTTPS ? 'good' : 'bad') +' text-center">' + (hasHTTPS ? '✅' : '❌') + '</td>');
 
         // feeds
-        var feeds = false;
-        feeds = _.uniq(_.flatten(_.map(item.urlchecks, 'content.feeds')));
-        row.append('<td class="'+ (feeds.length ? 'good' : 'bad') +' text-center">' + (feeds.length ? '✅' : '❌') + '</td>');
+        var feeds = item.result.FEEDS.value;
+        row.append('<td class="'+ (feeds ? 'good' : 'bad') +' text-center">' + (feeds ? '✅' : '❌') + '</td>');
 
         // screenshots
         var screenshot = false;
-        if (item.canonical_urls.length > 0) {
-          if (typeof screenshots[item.canonical_urls[0]] !== 'undefined') {
-            var surl = 'http://green-spider-screenshots.sendung.de/320x640/'+screenshots[item.canonical_urls[0]];
-            var lurl = 'http://green-spider-screenshots.sendung.de/1500x1500/'+screenshots[item.canonical_urls[0]];
+        if (item.details.canonical_urls.length > 0) {
+          if (typeof screenshots[item.details.canonical_urls[0]] !== 'undefined') {
+            var surl = 'http://green-spider-screenshots.sendung.de/320x640/'+screenshots[item.details.canonical_urls[0]];
+            var lurl = 'http://green-spider-screenshots.sendung.de/1500x1500/'+screenshots[item.details.canonical_urls[0]];
             screenshot = '<a class="screenshot" href="'+ surl +'" target="_blank" title="Mobile">M</a>';
             screenshot += '<a class="screenshot" href="'+ lurl +'" target="_blank" title="Desktop">D</a>';
           }
