e09bf7a4d4d037b7b88ee21f89514d6a3e15a380
Marian Steinbach <marian@giantswarm.io>
2018-05-04 00:38:44 +0200

    Add basic test for check_content

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import unittest
+import requests
+import responses
 import spider
 
 class TestSpider(unittest.TestCase):
@@ -23,6 +25,39 @@ class TestSpider(unittest.TestCase):
         expected_result = ['five', 'one']
         result = spider.reduce_urls(testdata)
         self.assertEqual(result, expected_result)
+
+    @responses.activate
+    def test_check_content1(self):
+        """
+        Very basic test of our content analysis function
+        """
+        url = 'http://my.url'
+        responses.add(responses.GET, url, status=200,
+            content_type='text/html',
+            body='''
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>The title</title>
+                </head>
+                </html>
+            ''')
+        r = requests.get(url)
+        result = spider.check_content(r)
+
+        del result['html']  # don't want to have the messy HTML part in comparison
+
+        expected_result = {
+            'icon': None,
+            'title': 'The title',
+            'generator': None,
+            'feeds': [],
+            'encoding': 'ISO-8859-1',
+            'canonical_link': None,
+            'opengraph': None
+        }
+        self.assertDictEqual(result, expected_result)
 
 
 if __name__ == '__main__':
