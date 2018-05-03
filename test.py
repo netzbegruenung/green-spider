7dce33980170dab4bdf9c8ee843a1380e6699286
Marian Steinbach <marian@giantswarm.io>
2018-05-03 13:55:34 +0200

    Add unit test for reduce_urls function

--- test.py
+++ test.py
@@ -3,15 +3,27 @@ import spider
 
 class TestSpider(unittest.TestCase):
 
-    def test_derive_test_hostnames1(self):
+    def test_derive_test_hostnames(self):
+        # case 1
         hn = spider.derive_test_hostnames('www.my-domain.de')
         expected = ['my-domain.de', 'www.my-domain.de']
         self.assertEqual(hn, expected)
-
-    def test_derive_test_hostnames2(self):
+        # case 2
         hn = spider.derive_test_hostnames('domain.de')
         expected = ['domain.de', 'www.domain.de']
         self.assertEqual(hn, expected)
+
+    def test_reduce_urls(self):
+        # This is our testdata
+        testdata = [
+            {'url': 'one', 'error': None, 'redirects_to': None},
+            {'url': 'two', 'error': 'Yes', 'redirects_to': None},
+            {'url': 'three', 'error': None, 'redirects_to': 'five'},
+        ]
+        expected_result = ['five', 'one']
+        result = spider.reduce_urls(testdata)
+        self.assertEqual(result, expected_result)
+
 
 if __name__ == '__main__':
     unittest.main()
