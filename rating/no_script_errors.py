afcb54374e6f5c101ad91f706665bb4589a02c45
Marian Steinbach <marian@giantswarm.io>
2018-10-01 08:14:42 +0200

    New rating: no script errors

---
+++ rating/no_script_errors.py
@@ -0,0 +1,42 @@
+"""
+If all URLs could be loaded without JavaScript errors, this rater gives a score.
+"""
+
+from rating.abstract_rater import AbstractRater
+
+class Rater(AbstractRater):
+
+    rating_type = 'boolean'
+    default_value = False
+    depends_on_checks = ['responsive_layout']
+    max_score = 1
+
+    def __init__(self, check_results):
+        super().__init__(check_results)
+    
+    def rate(self):
+        value = self.default_value
+        score = 0
+
+        found_pageloads = 0
+        found_errors = 0
+        for url in self.check_results['responsive_layout']:
+            if self.check_results['responsive_layout'][url]['logs'] == []:
+                found_pageloads += 1
+                continue
+            
+            # scan log entries for script errors
+            for entry in self.check_results['responsive_layout'][url]['logs']:
+                if entry['source'] == 'javascript':
+                    found_errors += 1
+
+        if found_pageloads > 0 and found_errors == 0:
+            value = True
+            score = self.max_score
+
+        return {
+            'type': self.rating_type,
+            'value': value,
+            'score': score,
+            'max_score': self.max_score,
+        }
