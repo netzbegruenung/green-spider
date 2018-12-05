7ecaa7587bbc9a9aebc5c896ab0c2e270c0851d0
Marian Steinbach <marian@giantswarm.io>
2018-12-05 21:25:03 +0100

    Extend check for Urwahl3000 theme

--- checks/generator.py
+++ checks/generator.py
@@ -74,7 +74,8 @@ class Checker(AbstractChecker):
         elif 'gruenes-internet.de' in page_content['content']:
             generator = 'wordpress-gruenes-internet'
 
-        elif 'Urwahl3000' in page_content['content']:
+        elif ('Urwahl3000' in page_content['content'] or
+            '/themes/urwahl3000' in page_content['content']):
             generator = 'wordpress-urwahl'
 
         elif ('josephknowsbest' in page_content['content'] or
