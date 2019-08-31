ec45dc68a12ee030e6c9b8090439ebd9a4364809
Chris Adams <chris@productscience.co.uk>
2019-08-31 12:47:12 +0200

    Add test for the green and a grey powered site

--- checks/green_power_test.py
+++ checks/green_power_test.py
@@ -6,24 +6,23 @@ from checks.config import Config
 class TestGreenPower(unittest.TestCase):
 
     def test_is_green(self):
-        """Checks that we get a green result for green powered site"""
+        """Check that we get a green result for green powered site"""
         # google is green, Koch Industries. predictably is not
         # TODO mock the result, so we don't need to hit a live API
         config = Config(urls=['https://google.com/'])
         checker = green_power.Checker(config=config)
         result = checker.run()
-        urls_after = checker.config.urls
-        print(urls_after)
+        self.assertEqual(result['https://google.com/'], True)
+
 
         self.assertEqual(1, 1)
 
     def test_is_not_green(self):
-        """Checks that we get a grey result for grey powered site"""
+        """Check that we get a grey result for grey powered site"""
         config = Config(urls=['http://www.kochind.com/'])
         checker = green_power.Checker(config=config)
         result = checker.run()
-        urls_after = checker.config.urls
-        print(urls_after)
+        self.assertEqual(result['http://www.kochind.com/'], False)
 
 
 
