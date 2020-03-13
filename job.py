e8770738292fd838b865504289b7f2975f7c0bb4
Marian Steinbach <marian@giantswarm.io>
2020-03-13 21:35:43 +0100

    Report resource usage stats for each job

--- job.py
+++ job.py
@@ -8,6 +8,8 @@ import json
 import os
 from datetime import datetime
 import time
+import sys
+import logging
 
 import docker
 
@@ -20,6 +22,7 @@ DOCKER_IMAGE = 'quay.io/netzbegruenung/green-spider:latest'
 CREDENTIALS_PATH = '/secrets/datastore-writer.json'
 
 client = docker.from_env()
+low_level_client = docker.APIClient(base_url='unix://var/run/docker.sock')
 
 pwd = os.path.abspath(".")
 secrets_path = pwd + "/secrets"
@@ -29,8 +32,17 @@ volumes = {}
 volumes[secrets_path] = {'bind': '/secrets', 'mode': 'ro'}
 volumes[chromedir_path] = {'bind': '/opt/chrome-userdir', 'mode': 'rw'}
 
+logger = logging.getLogger('rq.worker')
+logger.setLevel(logging.DEBUG)
 
 def run(job):
+    """
+    Runs a spider container with the given job.
+
+    Returns the container logs. If the execution takes longer than the
+    duration defined by the JOB_TIMEOUT environment variable (in seconds),
+    the container gets killed.
+    """
     cmd_template = ("python cli.py --credentials-path={path} "
                     " --loglevel=debug "
                     " spider "
@@ -51,28 +63,79 @@ def run(job):
 
     id = container.id
 
-    logs = ""
+    results = {
+        'url': job['url'],
+        'logs': '',
+        'stats': {
+            'cpu_usage_seconds': 0,
+            'network_received_bytes': 0,
+            'network_transmitted_bytes': 0,
+            'memory_max_bytes': 0,
+        },
+    }
 
     # wait for finish
     start = datetime.utcnow()
     while True:
-        time.sleep(5)
+        time.sleep(1)
 
         clist = client.containers.list(filters={'id': id})
         if len(clist) == 0:
             break
 
         for c in clist:
+
+            # Collect stats
+            try:
+                stats = low_level_client.stats(id, stream=False)
+                #logger.debug("Stats: %s" % json.dumps(stats, indent=2))
+
+                cpu_usage = stats['cpu_stats']['cpu_usage']['total_usage'] / 1000000000.0
+                if 'networks' in stats:
+                    network_received_bytes = stats['networks']['eth0']['rx_bytes']
+                    network_transmitted_bytes = stats['networks']['eth0']['tx_bytes']
+                
+                memory_max_bytes = 0
+                if 'max_usage' in stats['memory_stats']:
+                    memory_max_bytes = stats['memory_stats']['max_usage']
+                    results['stats']['memory_max_bytes'] = memory_max_bytes
+
+                if cpu_usage > 0:
+                    results['stats']['cpu_usage_seconds'] = cpu_usage
+                    logger.debug("Stats: CPU time %d Sec, RX %d KB, Mem %d MB" % (cpu_usage, network_received_bytes/1000, memory_max_bytes/1000000))
+                
+                if network_received_bytes > 0:
+                    results['stats']['network_received_bytes'] = network_received_bytes
+
+                if network_transmitted_bytes > 0:
+                    results['stats']['network_transmitted_bytes'] = network_transmitted_bytes
+                
+                
+
+            except docker.errors.APIError as e:
+                logger.error("Could not get stats: %s" % e)
+            except json.decoder.JSONDecodeError:
+                # This means we didn't get proper stats
+                pass
+            
+            # Collect logs
+            # try:
+            #     results['logs'] = c.logs()
+            # except docker.errors.NotFound:
+            #     pass
+            # except docker.errors.APIError:
+            #     pass
+
             if c.status != "running":
-                print("Container %s status: %s" % (c.id, c.status))
+                logger.info("Container %s status: %s" % (c.id, c.status))
 
             if c.status == "exited":
+                logger.debug("Container %s is exited." % c.id)
                 break
 
             runtime = (datetime.utcnow() - start).seconds
             if runtime > TIMEOUT:
-                logs = container.logs()
                 c.kill()
                 raise Exception("Execution took too long. Killed container after %s seconds." % TIMEOUT)
 
-    return logs
+    return results
